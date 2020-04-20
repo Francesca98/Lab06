e95088d381d7f4051579553ff64992ea280c464d
francescacatalano <francescacatalano@macbook-air.station>
2020-04-21 00:20:18 +0200

    1

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/FXMLController.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/FXMLController.java
@@ -5,6 +5,12 @@
 package it.polito.tdp.meteo;
 
 import java.net.URL;
+import java.time.Month;
+
+import it.polito.tdp.meteo.model.*;
+
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,6 +19,8 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	Model model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -21,7 +29,7 @@ public class FXMLController {
     private URL location;
 
     @FXML // fx:id="boxMese"
-    private ChoiceBox<?> boxMese; // Value injected by FXMLLoader
+    private ChoiceBox<Month> boxMese; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnUmidita"
     private Button btnUmidita; // Value injected by FXMLLoader
@@ -47,8 +55,26 @@ public class FXMLController {
         assert boxMese != null : "fx:id=\"boxMese\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnUmidita != null : "fx:id=\"btnUmidita\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injecited: check your FXML file 'Scene.fxml'.";
 
     }
+   
+
+	public void setModel(Model m) {
+		// TODO Auto-generated method stub
+		this.model=m;
+		setChoose();
+		
+	}
+	private void setChoose()
+	{
+		for(int i=1;i<=12;i++)
+		{
+			this.boxMese.getItems().add(Month.of(i)) ;
+			
+		}
+
+		
+		}
 }
 
